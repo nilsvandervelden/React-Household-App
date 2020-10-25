6466baa21d310ce27e673d3b412c6bfb44b98ab5
Nils van der Velden <nilsepils123@gmail.com>
2020-10-25 11:03:33 +0100

    Stange error in RecipeList

--- src/components/recipe/RecipeList.js
+++ src/components/recipe/RecipeList.js
@@ -1,7 +1,78 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchRecipes } from '../../actions';
 
-const RecipeList = () => {
-    return <div>RecipeList</div>;
+class RecipeList extends React.Component {
+    componentDidMount() {
+        this.props.fetchRecipes();
+    }
+
+    renderAdmin(recipe) {
+        if (recipe.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <Link to={`/recipe/edit/${recipe.id}`} className="ui button primary">
+                        EDIT
+                    </Link>
+                    <Link
+                        to={`/recipes/delete/${recipe.id}`}
+                        className="ui button negative"
+                    >
+                        Delete
+                    </Link>
+                </div>
+            );
+        }
+    }
+
+    renderList() {
+        return this.props.recipes.map(recipe => {
+            return (
+                <div className="item" key={recipe.id}>
+                    {this.renderAdmin(recipe)}
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {recipe.title}
+                        <div className="description">{recipe.description}</div>
+                    </div>
+                </div>
+            );
+        });
+    }
+
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
+                    <Link to="/recipes/new" className="ui button primary">
+                        Create Recipe
+                    </Link>
+                </div>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Recipes</h2>
+                <div className="ui celled list">{this.renderList()}</div>
+                {this.renderCreate()}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        streams: Object.values(state.recipes),
+        currentUserId: state:auth.userId,
+        isSignedIn: state.auth.isSignedIn
+    };
 };
 
-export default RecipeList;
+export default connect(
+    mapStateToProps,
+    { fetchRecipes }
+  )(RecipeList);
