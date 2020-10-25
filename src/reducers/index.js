ac17ecdbcd98b0c68496b5551eaf066df675f383
Nils van der Velden <nilsepils123@gmail.com>
2020-10-25 10:22:01 +0100

    Fixed error, can now add basic recipe

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form'; 
 import authReducer from './authReducer';
+import recipeReducer from './recipeReducer';
 
 export default combineReducers({
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    recipes: recipeReducer
 });
