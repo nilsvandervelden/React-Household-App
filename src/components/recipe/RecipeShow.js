27175e8f42b771569efe7b0ef0a1e3baa3a071c2
Nils van der Velden <nilsepils123@gmail.com>
2020-10-27 16:13:21 +0100

    Trying to display recipes...

--- src/components/recipe/RecipeShow.js
+++ src/components/recipe/RecipeShow.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchRecipe } from '../../actions';
 
-const RecipeShow = () => {
-    return <div>RecipeShow</div>;
-};
+class RecipeShow extends React.Component{
+    componentDidMount() {
+        this.props.fetchRecipe(this.props.match.params.id);
+    }
 
-export default RecipeShow;
+    renderIngredients() {
+        const recipeArray = this.props.recipe.step;
+        var index
+        for (index = 0; index <= recipeArray.length; index++) {
+            console.log(recipeArray[index][0])
+        }
+    }
+
+    render() {
+        if (!this.props.recipe) {
+            return <div>Loading...</div>
+        }
+
+        const {title, description} = this.props.recipe;
+
+        return (
+            <div>
+                <h1>{this.props.recipe.title} </h1>
+                <h5>{this.props.recipe.description} </h5>
+                {/* <h5>{recipeArray[0][0]} </h5> */}
+                <h5>{this.renderIngredients()}</h5>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { recipe: state.recipes[ownProps.match.params.id] };
+;}
+
+export default connect(
+    mapStateToProps,
+    { fetchRecipe }
+)(RecipeShow);
