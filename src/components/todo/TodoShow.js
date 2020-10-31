704e0cc3b8b452c6c29d632e760093761d6dfced
Nils van der Velden <nilsepils123@gmail.com>
2020-10-31 10:39:35 +0100

    Added TodoShow component

--- src/components/todo/TodoShow.js
+++ src/components/todo/TodoShow.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchTodo } from '../../actions';
 
-const TodoShow = () => {
-  return <div>TodoShow</div>;
+class TodoShow extends React.Component{
+
+    componentDidMount() {
+        this.props.fetchTodo(this.props.match.params.id);
+    }
+
+    render () {
+        if(!this.props.todo) {
+            return <div>Loading...</div>
+        }
+
+        const { title, description } = this.props.todo;
+
+        return (
+            <div>
+                <h1> {title} </h1>
+                <h5> {description} </h5>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { todo: state.todos[ownProps.match.params.id]};
 };
 
-export default TodoShow;
+export default connect(mapStateToProps, { fetchTodo})(TodoShow);
