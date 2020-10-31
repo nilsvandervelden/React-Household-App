59c5bf6abd1b972811410349f8eaa4d2aa74b46b
Nils van der Velden <nilsepils123@gmail.com>
2020-10-31 09:26:23 +0100

    Fixed some indentation

--- src/components/todo/TodoEdit.js
+++ src/components/todo/TodoEdit.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TodoEdit = () => {
-  return <div>TodoEdit</div>;
+    return <div>TodoEdit</div>;
 };
 
 export default TodoEdit;
--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchTodos } from '../../actions';
+import { Link } from 'react-router-dom';
+
+class TodoList extends React.Component {
+    componentDidMount() {
+        this.props.fetchTodos();
+  }
+
+    renderAdmin(todo) {
+        if (todo.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                
+        
+                </div>
+            )
+        }
+    }
+}
 
 const TodoList = () => {
-  return <div>TodoList</div>;
+    return <div>TodoList</div>;
 };
 
 export default TodoList;
