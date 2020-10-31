4d9c72d2516500afb283a552d9ddc9b96b31aa98
Nils van der Velden <nilsepils123@gmail.com>
2020-10-31 09:58:51 +0100

    Added Todo edit component

--- src/components/todo/TodoEdit.js
+++ src/components/todo/TodoEdit.js
@@ -1,7 +1,38 @@
+import _ from 'lodash';
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchTodo, editTodo } from '../../actions';
+import TodoForm from './TodoForm';
+import { formValueSelector } from 'redux-form';
 
-const TodoEdit = () => {
-    return <div>TodoEdit</div>;
+class TodoEdit extends React.Component {
+    componentDidMount(){
+        this.props.fetchTodo(this.props.match.params.id);
+    }
+
+    onSubmit = formValues => {
+        this.props.editTodo(this.props.match.params.id, formValues);
+    };
+
+    render() {
+        if (!this.props.todo) {
+            return <div> Loading... </div>
+        }
+        return (
+            <div>
+                <h3> Edit a Todo </h3>
+                <TodoForm
+                    initialValues={_.pick(this.props.todo, 'title, description')}
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { todo: state.todos[ownProps.match.params.id]};
 };
 
-export default TodoEdit;
+export default connect(mapStateToProps, { fetchTodo, editTodo })(TodoEdit);
+
