3206060c390fbe896b03870c6df1de901d0813f2
Nils van der Velden <nilsepils123@gmail.com>
2020-10-31 09:50:51 +0100

    Added delete todo component

--- src/components/todo/TodoDelete.js
+++ src/components/todo/TodoDelete.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import Modal from '../Modal';
+import history from '../../history';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchTodo, deleteTodo } from '../../actions';
 
-const TodoDelete = () => {
-    return <div>TodoDelete</div>;
+class TodoDelete extends React.Component {
+    componentDidMount() {
+        this.props.fetchTodo(this.props.match.params.id);
+    }
+
+    renderActions() {
+        const id = this.props.match.params.id;
+        return (
+            <React.Fragment>
+                <button onClick={() => this.props.deleteTodo(id)} className="ui button negative">Delte</button>
+                <Link to="/" className="ui button">Cancel</Link>
+            </React.Fragment>
+        );
+    }
+
+    renderContent() {
+        if(!this.props.todo) {
+            return 'Are you sure you want to delte this todo?'
+        }
+        return `Are you sure you want to delte the todo with title: ${this.props.todo.title}`
+    }
+
+    render() {
+        return(
+            <Modal
+                title="Delete Todo"
+                content={this.renderContent()}
+                actions={this.renderActions()}
+                onDismiss={() => history.push('/')}
+            />
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { todo: state.todos[ownProps.match.params.id]}
 };
 
-export default TodoDelete;
+export default connect(mapStateToProps, { fetchTodo, deleteTodo })(TodoDelete);
