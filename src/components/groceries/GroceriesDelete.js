62450df4da8a9c10e52b704e51335e7c040cc7dd
Nils van der Velden <nilsepils123@gmail.com>
2020-11-01 20:47:30 +0100

    Added DeleteGroceries Component

---
+++ src/components/groceries/GroceriesDelete.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Modal from '../Modal';
+import history from '../../history';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchGrocieries, deleteGrocieries } from '../../actions';
+
+class GroceriesDelete extends React.Component {
+    componentDidMount() {
+        this.props.fetchGrocieries(this.props.match.params.id);
+    }
+
+    renderActions() {
+        const id = this.props.match.params.id;
+        return (
+            <React.Fragment>
+                <button onClick={() => this.props.deleteGrocieries(id)} className="ui button negative">Delete</button> 
+                <Link to="/" className="ui button"> Cancel </Link>
+            </React.Fragment>
+        );
+    }
+
+    renderContent() {
+        if(!this.props.groceries) {
+            return 'Are you sure you want to delete this product?'
+        }
+        return `Are you sure you want to delete the product with title: ${this.props.groceries.title}`
+    }
+    
+    render(){
+        return (
+            <Modal
+                title="Delete Product"
+                content={this.renderContent()}
+                actions={this.renderActions()}
+                onDismiss={() => history.push('/groceries')}
+            />
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { groceries: state.groceries[ownProps.match.params.id]}
+};
+
+export default connect(mapStateToProps, {fetchGrocieries, deleteGrocieries})(GroceriesDelete);
