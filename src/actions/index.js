8c06143ef4ef76e62aa3c7ab3a156e58992a6a4c
Nils van der Velden <nilsepils123@gmail.com>
2020-11-02 20:21:25 +0100

    Added action creaters

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import history from '../history'
 import {
     SIGN_IN,
     SIGN_OUT,
-    
+
     CREATE_RECIPE,
     FETCH_RECIPE,
     FETCH_RECIPES,
@@ -15,6 +15,12 @@ import {
     FETCH_TODO,
     EDIT_TODO,
     DELETE_TODO,
+
+    CREATE_GROCERIES,
+    FETCH_GROCERIES,
+    FETCH_ALL_GROCERIES,
+    EDIT_GROCERIES,
+    DELETE_GROCERIES,
 
 }   from './types';
 
@@ -99,4 +105,40 @@ export const deleteTodo = id => async dispatch => {
 
     dispatch({ type: DELETE_TODO, payload: id });
     history.push('/todos')
+};
+
+// GROCERIES
+
+export const createGroceries = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await api.post('/groceries', {...formValues, userId });
+
+    dispatch({ type: CREATE_GROCERIES, payload: response.data });
+    history.push('/groceries');
+};
+
+export const fetchAllGroceries = () => async dispatch => {
+    const response = await api.get('/groceries');
+  
+    dispatch({ type: FETCH_ALL_GROCERIES, payload: response.data });
+};
+
+export const fetchGroceries = (id) => async dispatch => {
+    const response = await api.get(`/groceries/${id}`);
+  
+    dispatch({ type: FETCH_GROCERIES, payload: response.data });
+};
+
+export const editGroceries= (id, formValues) => async dispatch => {
+    const response = await api.patch(`/groceries/${id}`, formValues);
+
+    dispatch({ type: EDIT_GROCERIES, payload: response.data });
+    history.push('/groceries');
+};
+
+export const deleteTodo = id => async dispatch => {
+    await api.delete(`/groceries/${id}`);
+
+    dispatch({ type: DELETE_GROCERIES, payload: id });
+    history.push('/groceries');
 };
