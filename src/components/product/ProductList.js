8884a1db6161942486037b68460eebcbc3bdf42c
Nils van der Velden <nilsepils123@gmail.com>
2020-11-06 08:03:05 +0100

    Adding styling to products

--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux'; 
 import { fetchProducts } from '../../actions';
 import { Link } from 'react-router-dom';
+import '../../stylesheets/Product.css';
 
 class ProductList extends React.Component {
     componentDidMount(){
@@ -24,18 +25,36 @@ class ProductList extends React.Component {
     }
 
     renderList () {
-        return this.props.products.map(products => {
+        return this.props.products.map(product => {
             return (
-                <div className="item" key={products.id}>
-                    {this.renderAdmin(products)}
-                    <i className="large middle aligned icon camera" />
+                <div className="card" key={product.id}>
+                    <div className="image">
+                        <img src={product.url}></img>
+                    </div>
                     <div className="content">
-                        <Link to={`/products/${products.id}`} className="header">
-                            {products.title}
-                        </Link>
-                        <div className="description">{products.description}</div>
+                        <a className="text">
+                            <div className="ui grid">
+                                <div className="eight wide column"> 
+                                    <h1>{product.title}</h1>
+                                </div>
+                                <div className="eight wide column">  
+                                    <h3>{product.price}</h3>
+                                </div>
+                            </div>
+                        </a>
                     </div>
                 </div>
+
+                //     </div>
+                //     {this.renderAdmin(product)}
+                //     <i className="large middle aligned icon camera" />
+                //     <div className="content">
+                //         <Link to={`/products/${product.id}`} className="header">
+                //             {product.title}
+                //         </Link>
+                //         <div className="description">{product.description}</div>
+                //     </div>
+                // </div>
             );
         });
     }
@@ -56,7 +75,7 @@ class ProductList extends React.Component {
         return (
             <div> 
                 <h2> Product </h2>
-                <div className="ui celled list"> {this.renderList()} </div>
+                <div className="ui five stackable cards"> {this.renderList()} </div>
                 {this.renderCreate()}
             </div>
         ); 
